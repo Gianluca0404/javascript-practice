c18f7ec35cbd6063ec2d994f72e51a1e22ee0ed1
Gianluca <gianluca.marcone00@libero.it>
2025-08-19 17:27:17 +0200

    ripasso.js

--- ripasso.js
+++ ripasso.js
@@ -2,28 +2,28 @@
 let nome = "Gianluca";
 let cognome = "Marcone";
 let nomeCompleto = nome + " " + cognome;
-console.log("1. Nome completo:", nomeCompleto);
+console.log("Nome completo:", nomeCompleto);
 
 // STRINGHE
 let frase = "JavaScript è divertente";
-console.log("2. Lunghezza frase:", frase.length);
-console.log("3. Frase maiuscola:", frase.toUpperCase());
+console.log("Lunghezza frase:", frase.length);
+console.log("Frase maiuscola:", frase.toUpperCase());
 
 // ARRAY
 let frutti = ["Mela", "Banana", "Kiwi"];
-console.log("4. Secondo frutto:", frutti[1]);
+console.log("Secondo frutto:", frutti[1]);
 frutti.push("Arancia");
-console.log("5. Array aggiornato:", frutti);
+console.log("Array aggiornato:", frutti);
 
 // CICLI FOR
-console.log("6. Numeri da 1 a 5:");
+console.log("Numeri da 1 a 5:");
 for (let i = 1; i <= 5; i++) {
   console.log(i);
 }
 
 // FOR...OF
 let animali = ["cane", "gatto", "leone"];
-console.log("7. Lista animali:");
+console.log("Lista animali:");
 for (let animale of animali) {
   console.log(animale);
 }
@@ -32,13 +32,13 @@ for (let animale of animali) {
 function saluta(nome) {
   return "Ciao, " + nome + "!";
 }
-console.log("8. Saluto:", saluta("Gianluca"));
+console.log("Saluto:", saluta("Gianluca"));
 
 // FUNZIONI CON PIÙ PARAMETRI E RETURN
 function somma(a, b) {
   return a + b;
 }
-console.log("9. Somma:", somma(4, 6));
+console.log("Somma:", somma(4, 6));
 
 // OGGETTI SEMPLICI
 let persona = {
@@ -46,7 +46,7 @@ let persona = {
   eta: 25,
   citta: "Napoli"
 };
-console.log("10. Città:", persona.citta);
+console.log("Città:", persona.citta);
 
 // OGGETTI CON METODI
 let utente = {
@@ -56,7 +56,7 @@ let utente = {
     return "Ciao, sono " + this.nome + " " + this.cognome;
   }
 };
-console.log("11. Presentazione:", utente.presentati());
+console.log("Presentazione:", utente.presentati());
 
 // ARRAY DI OGGETTI
 let prodotti = [
@@ -64,6 +64,7 @@ let prodotti = [
   { nome: "Pane", prezzo: 1 },
   { nome: "Acqua", prezzo: 0.5 }
 ];
-console.log("12. Prezzo del secondo prodotto:", prodotti[1].prezzo);
+console.log("Prezzo del secondo prodotto:", prodotti[1].prezzo);
+
 
 
